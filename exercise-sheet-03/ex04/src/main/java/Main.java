2f867c394e248869c7650a382c98f98eaf4e5f0e
srchby <leandrocdefarias@proton.me>
2025-08-12 22:05:58 -0300

    redid ex04, didnt read again

--- exercise-sheet-03/ex04/src/main/java/Main.java
+++ exercise-sheet-03/ex04/src/main/java/Main.java
@@ -1,17 +1,31 @@
 package main.java;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int[] nums = { 1, 6, 8, 2, 5 };
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> nums = new ArrayList<>();
+		System.out.println("Insira notas (-1 para parar): ");
+		while (true) {
+			int num = sc.nextInt();
+			if (num == -1) {
+				break;
+			}
+			nums.add(num);
+		}
+		System.out.println("Média: ");
 		System.out.println(mediaNotas(nums));
+		sc.close();
 	}
 
-	public static double mediaNotas(Double[] nums) {
-		return Arrays.stream(nums).mapToDouble(Double::doubleValue).reduce(0.0, (a, b) -> a + b)
-				/ (double) nums.length;
+	public static double mediaNotas(ArrayList<Integer> nums) {
+		if (nums.isEmpty()) {
+			return 0.0;
+		}
+		return nums.stream().mapToDouble(Integer::doubleValue).reduce(0.0, (a, b) -> a + b)
+				/ nums.size();
 	}
 }
